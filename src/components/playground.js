05e53461d6007103fa2704c8917f00768a7194d0
vitalii kalinin <vitaliik@civicresource.com>
2017-03-04 12:08:34 -0800

    add very basic board functions

--- src/components/playground.js
+++ src/components/playground.js
@@ -5,38 +5,67 @@ import {
 	TouchableOpacity,
 	Text,
 } from 'react-native';
+import Svg, {
+		Circle,
+		Rect,
+} from 'react-native-svg';
 
 const styles = StyleSheet.create({
 	MainContainer: {
 		flex: 1,
 		alignSelf: 'center',
-		justifyContent:'center',
+		justifyContent: 'center',
 	},
 	Row: {
 		flexDirection: 'row',
-		minHeight: 50,
 	},
 	Cell: {
 		borderColor: 'green',
-		minWidth: 50,
 	},
 	CellText: {
-		fontSize: 80 ,
+		fontSize: 80,
 	},
 });
 export default class Playground extends Component {
 
 	cellStyle(i, j, centerCell) {
-		const borderTopWidth = (i === centerCell ? 1 : 0);
-		const borderBottomWidth = (i === centerCell ? 1 : 0);
-		const borderLeftWidth = (j === centerCell ? 1 : 0);
-		const borderRightWidth = (j === centerCell ? 1 : 0);
+		const borderTopWidth = (i === centerCell ? 1 : null);
+		const borderBottomWidth = (i === centerCell ? 1 : null);
+		const borderLeftWidth = (j === centerCell ? 1 : null);
+		const borderRightWidth = (j === centerCell ? 1 : null);
 		return {
 			borderBottomWidth,
 			borderTopWidth,
 			borderLeftWidth,
 			borderRightWidth,
 		};
+	}
+	boardFigure(type) {
+		if (type === 1) {
+			return (
+					<Circle
+						cx="50"
+						cy="50"
+						r="30"
+						stroke="red"
+						strokeWidth="1"
+						fillOpacity="0"
+					/>
+			);
+		} else if (type === 2) {
+			return (
+					<Rect
+						x="15"
+						y="15"
+						width="70"
+						height="70"
+						stroke="red"
+						strokeWidth="1"
+						fillOpacity="0"
+					/>
+			);
+		}
+		return null;
 	}
 	render() {
 		const { board, onSelect } = this.props;
@@ -58,9 +87,14 @@ export default class Playground extends Component {
 								styles.Cell,
 								this.cellStyle(indexI, indexJ, centerCell),
 							]}
-							onPress={() => { this.props.onSelect({ x: indexI, y: indexJ }, 'green')}}
+							onPress={() => { this.props.onSelect({ x: indexI, y: indexJ }, j)}}
 						>
-							<Text style={styles.CellText}>{j}</Text>
+							<Svg
+								height="100"
+								width="100"
+							>
+							{this.boardFigure(j)}
+							</Svg>
 						</TouchableOpacity>
 					))}
 				</View>
--- src/containers/playgroundContainer.js
+++ src/containers/playgroundContainer.js
@@ -9,6 +9,7 @@ export default class PlaygroundContainer extends Component {
 		this.onSelect = this.onSelect.bind(this);
 		this.state = {
 			board: [],
+			player: 0,
 		};
 	}
 
@@ -16,11 +17,43 @@ export default class PlaygroundContainer extends Component {
 		this.createBoard(3);
 	}
 
+
+	isWinner(board, player) {
+		let result = false;
+		let rowScore = [0, 0, 0];
+		let columnScore = [0, 0, 0];
+		let diagScore = [0, 0];
+		for (let i = 0; i < board.length; i++) {
+			for (let j = 0; j < board.length; j++) {
+				if (board[i][j] === player) {
+					columnScore[i]++;
+					rowScore[j]++;
+					if (i === j) {
+						if (i < board.length / 2) {
+							diagScore[0]++;
+						} else if (i > board.length / 2) {
+							diagScore[1]++;
+						} else {
+							diagScore[0]++;
+							diagScore[1]++;
+						}
+					}
+				}
+			}
+		}
+		return columnScore.includes(3) || rowScore.includes(3) || diagScore.includes(3);
+	}
+
 	onSelect(coords, type, test) {
 		const updatedBoard = test || this.state.board.map(inner => inner.slice());
-		updatedBoard[coords.x][coords.y] = type;
 
-		this.setState({ board: updatedBoard });
+		const newPlayer = this.state.player === 1 ? 2 : 1;
+		updatedBoard[coords.x][coords.y] = newPlayer;
+
+		this.setState({ board: updatedBoard, player: newPlayer });
+		if (this.isWinner(updatedBoard, newPlayer)) {
+			alert('yay');
+		}
 	}
 
 	createBoard(n) {
@@ -28,12 +61,13 @@ export default class PlaygroundContainer extends Component {
 		for (let i = 0; i < n; i += 1) {
 			boardContructor[i] = [];
 			for (let j = 0; j < n; j += 1) {
-				boardContructor[i][j] = 'red';
+				boardContructor[i][j] = 0;
 			}
 		}
 
 		this.setState({ board: boardContructor });
 	}
+
 
 	render() {
 		return (<Playground board={this.state.board} onSelect={this.onSelect} />);
